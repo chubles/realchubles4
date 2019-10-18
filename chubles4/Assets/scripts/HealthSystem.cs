707ed18b0d921cdaef9911759b164318ce59afc5
jakob <jherrera2102@student.pps.net>
2019-10-18 10:54:41 -0700

    Healthsystem

---
+++ chubles4/Assets/scripts/HealthSystem.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+
+public class HealthSystem : MonoBehaviour
+{
+    public const int maxHealth = 100;
+    public int currentHealth = maxHealth;
+    public RectTransform healthbar;
+
+    public void TakeDamage(int amount)
+    {
+        currentHealth -= amount;
+        if (currentHealth <= 0)
+        {
+            currentHealth = 0;
+            Debug.Log("Dead");
+        }
+
+
+        //healthBar.sizeDelta = new Vector2(currentHealth * 2, healthBar.sizeDelta.y);
+    }
+
+
+
+}
