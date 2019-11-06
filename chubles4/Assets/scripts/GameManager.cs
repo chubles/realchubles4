ec9b99eab945d7991436ec93cb81328c15028c12
rory <awesomerortube@gmail.com>
2019-11-06 10:21:13 -0800

    Auto stash before merge of "master" and "origin/master"

---
+++ chubles4/Assets/scripts/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class GameManager : MonoBehaviour
+{
+    public int currentCake;
+    public Text cakeText; 
+        
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+
+
+    }
+    
+    public void AddCake(int cakeToAdd)
+    {
+        currentCake += cakeToAdd;
+        cakeText.text = "Cake score" + currentCake;
+    }
+}
