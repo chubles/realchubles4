69ac1c80b3f723f57ee4ad020eb560d21f795d21
jakob <jherrera2102@student.pps.net>
2020-03-02 10:37:12 -0800

    Completed Enemy Follow

---
+++ chubles4/Assets/scripts/EnemyFollow.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFollow : MonoBehaviour
+{
+    public int minRange;
+    private Vector3 targetTran;
+    private Transform target = null;
+    private float enemySpeed = 10;
+    void OnTriggerEnter(Collider other) {
+        if (other.tag == "Player") target = other.transform;
+    }
+ 
+    void OnTriggerExit(Collider other) {
+        if (other.tag == "Player") target = null;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (target == null) return;
+        transform.LookAt(target);
+        float distance = Vector3.Distance(transform.position, target.position);
+        bool tooClose = distance < minRange;
+        Vector3 direction = tooClose ? Vector3.back * enemySpeed : Vector3.forward * enemySpeed;
+        transform.Translate(direction * Time.deltaTime); 
+    }
+}
--- chubles4/Assets/scripts/Waypoints.cs
+++ chubles4/Assets/scripts/Waypoints.cs
@@ -7,53 +7,22 @@ using UnityEngine.AI;
 
 public class Waypoints : MonoBehaviour
 {
-    /*public GameObject[] waypoints;
+    public GameObject[] waypoints;
     int currentPos = 0;
     float rotSpeed;
     float WPradius = 1;
-    private float Speed = 5;*/
+    private float Speed = 5;
 
-    /*private NavMeshAgent Mob;
-    public GameObject Player;
-    public float MobDistanceRun = 4;*/
-    
-    
-    public int minRange;
-    private Vector3 targetTran;
-    private Transform target = null;
-    private float enemySpeed = 5;
-    void OnTriggerEnter(Collider other) {
-        if (other.tag == "Player") target = other.transform;
-    }
- 
-    void OnTriggerExit(Collider other) {
-        if (other.tag == "Player") target = null;
-    }
 
     void Start()
     {
-        /*target = GameObject.FindWithTag("Player");
-        targetTran = target.transform.position;*/
+        
     }
 
     private void Update()
     {
-        /*if ((Vector3.Distance(transform.position, target.transform.position) < maxRange) &&
-            (Vector3.Distance(transform.position, target.transform.position) > minRange))
-        {
-            transform.LookAt(targetTran);
-            transform.Translate(Vector3.forward * Time.deltaTime);
-        }*/
 
-        if (target == null) return;
-        transform.LookAt(target);
-        float distance = Vector3.Distance(transform.position, target.position);
-        bool tooClose = distance < minRange;
-        Vector3 direction = tooClose ? Vector3.back * enemySpeed : Vector3.forward * enemySpeed;
-        transform.Translate(direction * Time.deltaTime);
-        
-        
-        /*if (Vector3.Distance(waypoints[currentPos].transform.position, transform.position) < WPradius)
+        if (Vector3.Distance(waypoints[currentPos].transform.position, transform.position) < WPradius)
         {
             currentPos++;
             if (currentPos >= waypoints.Length)
@@ -63,33 +32,9 @@ public class Waypoints : MonoBehaviour
         }
 
         transform.position = Vector3.MoveTowards(transform.position, waypoints[currentPos].transform.position,
-            Time.deltaTime * Speed);*/
+            Time.deltaTime * Speed);
         
     }
-
-    /*private void Start()
-    {
-        Mob = GetComponent<NavMeshAgent>();
-    }
-
-    private void Update()
-    {
-        float distance = Vector3.Distance(transform.position, Player.transform.position);
-
-        if (distance < MobDistanceRun)
-        {
-            Vector3 dirToPlayer = transform.position - Player.transform.position;
-            Vector3 newPos = transform.position - dirToPlayer;
-
-            Mob.SetDestination(newPos);
-        }
-    }
-
-    private void Update()
-    {
-        transform.LookAt(Player.transform);
-        transform.position += transform.forward * speed * Time.deltaTime;
-    }*/
 
 
 }
