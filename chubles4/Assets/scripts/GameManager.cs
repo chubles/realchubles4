72cea6c88483e1f8fd80020a964b38d7b2a5547e
Aaron <alindstedt03@gmail.com>
2020-03-05 10:06:48 -0800

    minor fix before demo

--- chubles4/Assets/scripts/GameManager.cs
+++ chubles4/Assets/scripts/GameManager.cs
@@ -34,7 +34,7 @@ public class GameManager : MonoBehaviour
        print("advance");
        if (nextLevel == true)
        {
-           if (EnemiesLeft + cakeLeft == 0)
+           if (EnemiesLeft + cakeLeft <= 0)
            {
                currentScene += 1;
                SceneManager.LoadScene(currentScene + 1)/*SceneManager.GetActiveScene().buildIndex + 1)*/;
