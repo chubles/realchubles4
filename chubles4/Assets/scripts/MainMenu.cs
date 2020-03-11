e86497eddb497e4212b77ecaa349c68979653ba7
rory <awesomerortube@gmail.com>
2020-03-11 10:07:05 -0700

    some invisa walls

---
+++ chubles4/Assets/scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public int startScene;
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(startScene)/*SceneManager.GetActiveScene().buildIndex + 1)*/;
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+    
+}
